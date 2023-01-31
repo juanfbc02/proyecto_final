a01172ee28d31f742306051e4792c9f1b71e1eb3
juanfbc02 <juanjfbc02@gmail.com>
2023-01-31 09:01:23 -0300

    se agrega el pryecto

---
+++ app.js
@@ -0,0 +1,15 @@
+
+alert ("bienvenidos a mi cv")
+
+function mostrar(){
+    document.getElementById('mostrar').style.display = 'block'
+}
+
+function mostrar2(){
+    document.getElementById('mostrar2').style.display = 'block'
+}
+
+function mostrar3(){
+    document.getElementById('mostrar3').style.display = 'block'
+}
+
